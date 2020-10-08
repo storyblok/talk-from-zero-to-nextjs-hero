5ac569fcd6d852585aabf808ea12149c2dbe6a0d
Samuel Snopko <samuel@snopko.com>
2020-10-08 13:50:42 +0200

    remove getStaticProps experiment

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,6 @@ import Page from '../components/Page'
 import Layout from '../components/Layout'
 import StoryblokService from '../utils/storyblok-service'
 
-// export async function getStaticProps(preview=true) {
-//   const res = await StoryblokService.get('cdn/stories/home')
-
-//   return {
-//     props: {
-//       story: res.data.story
-//     }, // will be passed to the page component as props
-//   }
-// }
-
 export default class extends React.Component {
   constructor(props) {
     super(props)
